5bdea6933c52b8f88fcb533d77aca70f58624c0c
oshimish <pustotahe.re@gmail.com>
2023-03-27 22:25:33 +0400

    add rake-api appInsights

--- backend/Rlake.Api/Program.cs
+++ backend/Rlake.Api/Program.cs
@@ -13,14 +13,16 @@ namespace Rlake.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddProblemDetails();
+            builder.Services.AddApplicationInsightsTelemetry();
 
             builder.Services.AddTransient<UploadFileService>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             //if (app.Environment.IsDevelopment())
             //{
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI();
             //}
